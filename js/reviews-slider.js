8df0b2b1339a8241e07acf8bfa5674078d372058
orezowt <sabjer20@gmail.com>
2024-12-19 19:03:07 +0300

    Слайдер с отзывами

---
+++ js/reviews-slider.js
@@ -0,0 +1,34 @@
+$(document).ready(function(){
+  $('.reviews-slider').slick({
+    infinite: true,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    dots: true,
+    responsive: [
+    {
+      breakpoint: 1281,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 768,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        dots: false
+      }
+    }
+    ]
+  });
+});
