b3cacfd6bc1aef25466852072a0d8c82f49e646b
orezowt <sabjer20@gmail.com>
2025-01-05 19:11:56 +0300

    Форма заявки

--- js/modal-window.js
+++ js/modal-window.js
@@ -1,37 +1,45 @@
 let btns_modal = document.querySelectorAll('*[data-modal]')
-let modal = document.querySelector('#modal-content')
-let modal_shadow = document.querySelector('#modal-shadow')
-let btn_close = document.querySelector("#button-close")
+let modal = document.querySelector('#modal-registration')
+let btn_close = document.querySelector("#modal-button-close")
 let html = document.documentElement;
 let scrollPosition
 
-function open_close(){
-	modal.classList.toggle('modal-invisible')
-	modal_shadow.classList.toggle('modal-invisible')
-	modal_shadow.classList.toggle('modal-visible')
-	modal.classList.toggle('modal-visible')
+function closeOnBackDropClick({currentTarget, target}){
+	const modal = currentTarget
+	const isClickedOnBackDrop = target === modal
+	if (isClickedOnBackDrop){
+		close()
+	}
+}
+
+function returnScroll(){
+	document.body.classList.remove('scroll-lock')
 }
 
 function open(){
 	scrollPosition = window.pageYOffset;
 	html.style.top = -scrollPosition + "px";
-	html.classList.add("modal-opened");
-	open_close()
+	modal.showModal()
+	document.body.classList.add('scroll-lock')
 }
 
 function close(){
-	html.classList.remove("modal-opened");
 	//прокручиваем окно туда где оно было
 	window.scrollTo({top: scrollPosition, left: 0, behavior:'instant'})
 	html.style.top = ""
-	open_close()
+	modal.close()
+	returnScroll()
 }
 
-window.onload = function(){
-	btns_modal.forEach(function(button){
-		button.onclick = open
-	})
+modal.addEventListener('click', closeOnBackDropClick)
+modal.addEventListener('cancel', (event)=>{
+	returnScroll()
+})
+btns_modal.forEach(function(button){
+	button.onclick = open
+})
 
-	btn_close.onclick = close
-	modal_shadow.onclick = close
-}
+btn_close.addEventListener('click', (event)=>{
+	event.stopPropagation()
+	close()
+})
