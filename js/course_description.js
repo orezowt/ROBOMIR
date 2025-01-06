8c89f2584a9f1f030859fdd987ab7eb338b6e01b
orezowt <sabjer20@gmail.com>
2025-01-06 13:55:34 +0300

    Адаптивность раздела с курсами

--- js/course_description.js
+++ js/course_description.js
@@ -1,32 +1,47 @@
 let btns = document.querySelectorAll(".button-read-more")
 let courses = document.querySelectorAll(".courses")
 
-// function FullDescription(btn){
-// 	let course = btn.previousElementSibling.previousElementSibling
-// 	course.classList.toggle("full-description")
-// }
+
+function switch_view(button, class_name){
+	let selected_course = button.parentNode.parentNode
+
+	// сбрасываем текст кнопок, если открываем описание нескольких курсов одновременно
+	btns.forEach(function(btn){
+		if (btn != button){
+			btn.innerHTML = "Узнать больше"
+		}
+	})
+
+	if (button.innerHTML == "Узнать больше"){
+		button.innerHTML = "Свернуть описание"
+	}
+	else{
+		button.innerHTML = "Узнать больше"
+	}
+	
+	// сбрасываем стили для блоков с курсами, 
+	// если открываем описание нескольких курсов одновременно
+	courses.forEach(function(course){
+		if (course != selected_course)
+			course.classList.remove(class_name)
+	})
+	
+	selected_course.classList.toggle(class_name)
+	selected_course.scrollIntoView(
+		{behaviour: 'smooth', block: 'center'}
+		)
+}
+
 
 btns.forEach(function(button){
 	button.onclick = function(){
+		let screen_width = window.innerWidth
 
-		if (button.innerHTML == "Узнать больше"){
-			button.innerHTML = "Свернуть описание"
+		if (screen_width < 600){
+			switch_view(button, "mb-full-description")
 		}
 		else{
-			button.innerHTML = "Узнать больше"
-		}
-
-		let selected_course = button.parentNode.parentNode
-		courses.forEach(function(course){
-
-			if (course != selected_course)
-				course.classList.remove("full-description")
-		})
-		
-		selected_course.classList.toggle("full-description")
-		selected_course.scrollIntoView(
-			{behaviour: 'smooth', block: 'center'}
-			)
-		
+			switch_view(button, "full-description")
+		}		
 	} 
 })
