a1d02844be595a5a3827d5d8cf3a9112d186a1d9
orezowt <sabjer20@gmail.com>
2025-04-17 15:20:53 +0300

    Доработана кнопка меню

--- js/nav-menu.js
+++ js/nav-menu.js
@@ -1,15 +1,18 @@
-let dropdown_menu = document.querySelector('ul.menu')
+let dropdown_menu = document.querySelector('ul.menu-container')
 let btn_nav_menu = document.querySelector('#mb-button-menu')
+let menu = document.querySelectorAll('.menu')
 
+
+
+ 
+document.addEventListener( 'mouseup', (e) => {
+	const withinBoundaries = e.composedPath().includes(menu[0]) || e.composedPath().includes(menu[1]);
+	
+	if (dropdown_menu.contains(e.target) || !withinBoundaries) {
+        dropdown_menu.style.display = 'none';
+    }
+})
 
 btn_nav_menu.addEventListener('click', ()=>{
 	(getComputedStyle(dropdown_menu).display == 'none') ? dropdown_menu.style.display = 'block' : dropdown_menu.style.display = 'none'
-})
- 
-document.addEventListener( 'mouseup', (e) => {
-	const withinBoundaries = e.composedPath().includes(dropdown_menu);
-
-	if (!dropdown_menu.contains(e.target) || withinBoundaries) {
-        dropdown_menu.style.display = 'none';
-    }
 })
